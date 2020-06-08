89803afcbf0dce4b0c774df287b064c49e46f09a
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-06-08 11:13:29 -0500

    final push before submission

--- src/App.js
+++ src/App.js
@@ -22,24 +22,20 @@ class App extends Component {
     };
 
     onClick = id => {
-
         // Filter for the clicked match
         const characters = this.state.characters
         const clickedCharacter = this.state.characters.filter(characters => characters.id === id);
 
         // If the matched image's value is true, start over
         if (clickedCharacter[0].click) {
-
             // shuffles characters
             characters.sort((a, b) => 0.5 - Math.random());
-
             // reset state
             this.setState({ topScore: this.state.score, score: 0, newTopScore: this.state.newTopScore });
 
             if (this.state.score > this.state.topScore) {
                 this.setState({ newTopScore: this.state.topScore });
             }
-
             for (var i = 0; i < characters.length; i++) {
                 characters[i].click = false;
             }
@@ -49,17 +45,12 @@ class App extends Component {
         } else if (this.state.score < 12) {
             // Set its value to true
             clickedCharacter[0].click = true;
-
             this.setState({ score: this.state.score + 1 });
-
             characters.sort((a, b) => 0.5 - Math.random());
-
         } else {
             // restarts if game is won
             this.setState({ score: 0, topScore: 0, newTopScore: 0 });
-
             characters.sort((a, b) => 0.5 - Math.random());
-
         }
 
     }
